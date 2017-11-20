c91ffe620d575a6fde6fdf9f125a7fde10da2509
Andreas Rasmussen <Andrea8900@hotmail.com>
2017-11-20 13:49:45 +0100

    Initial Commit

---
+++ EmojiDic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiDic
+//
+//  Created by Andreas Rasmussen on 20/11/2017.
+//  Copyright © 2017 Andreas Rasmussen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
